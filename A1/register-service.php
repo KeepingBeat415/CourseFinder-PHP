20dfdfd324235260ade7d6758ec7fb635436a4c5
Ms-BUG <sevenliu18@gmail.com>
2022-09-28 21:33:26 -0400

    validate-css-register-form

--- A1/register-service.php
+++ A1/register-service.php
@@ -1,43 +1,75 @@
-<?php 
+<?php
+    // Include config file
     include('config.php'); 
-
-    // Showing ERROR
+    // Display ERROR
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-   
-
+    // Initialize variables
     $username = $password = $first_name = $last_name = $address = "";
-    $email = $phone_number = $DOB = $user_type = "";
+    $email = $phone_number = $DOB = $user_type = $password_err = $username_err="";
 
-    // Processing form data when form is submitted
+    // Processing form data
     if($_SERVER["REQUEST_METHOD"] == "POST"){
+        
+        // Validate password whether match
+        if($_POST["password"] != $_POST["confirm_password"]){
+            $password_err = "Password did not match.";
+        }
+        // Prepare a select statement
+        $sql = "SELECT id FROM Users WHERE username = ?";
+        // Validate username whether exist
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            // Set parameters
+            $username = trim($_POST["username"]);
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "The Username is already existed.";
+                } 
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        // Close statement
+        mysqli_stmt_close($stmt);
 
-      $sql = "INSERT INTO Users (username, password, first_name, last_name, address, email, phone_number, DOB, user_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-              
-      if($stmt = mysqli_prepare($conn, $sql)){
-          // Bind variables to the prepared statement as parameters
-          mysqli_stmt_bind_param($stmt, "sssssssss", $username, $password, $first_name, $last_name, $email, $address, $phone_number, $DOB, $user_type);
-          // Set parameters
-          $username = $_POST["username"];
-          $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // Creates a password hash
-          $first_name = $_POST["first_name"];
-          $last_name = $_POST["last_name"];
-          $address = $_POST["address"];
-          $email = $_POST["email"];
-          $phone_number = $_POST["phone_number"];
-          $DOB = date('y-m-d', strtotime($_POST["DOB"]));
-          $user_type = $_POST["user_type"];
-
-          if(mysqli_stmt_execute($stmt)){
-              header("location: login.php");
-          } else{
-              echo "Something went wrong." . mysqli_stmt_error($stmt);
-          }
-          mysqli_stmt_close($stmt);
-      }
-      else{
-          echo "Something went wrong.";
-      }
-  }
+        if(empty($password_err) && empty($username_err)){
+            $sql = "INSERT INTO Users (username, password, first_name, last_name, address, email, phone_number, DOB, user_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            // Attempt to execute the prepared statement   
+            if($stmt = mysqli_prepare($conn, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "sssssssss", $username, $password, $first_name, $last_name, $email, $address, $phone_number, $DOB, $user_type);
+                // Set parameters
+                $username = $_POST["username"];
+                $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // Creates a password hash
+                $first_name = $_POST["first_name"];
+                $last_name = $_POST["last_name"];
+                $address = $_POST["address"];
+                $email = $_POST["email"];
+                $phone_number = $_POST["phone_number"];
+                $DOB = date('y-m-d', strtotime($_POST["DOB"]));
+                $user_type = $_POST["user_type"];
+                // Insert into database
+                if(mysqli_stmt_execute($stmt)){
+                    header("location: login.php");
+                } else{
+                    echo "Oops! Something went wrong." . mysqli_stmt_error($stmt);
+                }
+                // Close Statement
+                mysqli_stmt_close($stmt);
+            }
+            else{
+                echo "Oops! Something went wrong.";
+            }
+    
+        }
+    }
+    // Close connection
+    mysqli_close($conn);
 ?>
